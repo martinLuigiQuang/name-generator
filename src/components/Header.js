a1f941865340f616523cdc0158b6bd0dcf5a0578
Martin <martin.nguyen.20289@gmail.com>
2021-09-12 23:15:53 -0400

    added header component and basic placeholder styling; added labels in NameGenerator component and basic styling

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="company-name"></div>
+            <div className="event-banner"></div>
+        </header>
+    );
+};
+
+export default Header;
