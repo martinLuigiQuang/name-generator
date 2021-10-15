7b6809117972f12e46c11d10d46cf1a23444f3d1
Jonathan Lyn <jonlyn22@gmail.com>
2021-10-14 23:16:41 -0400

    updated enter name styling and added different language text to event name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import './Header.scss';
-import EventLogo from '../assets/Invincible Word Mark.png';
+import EventLogoEnglish from '../assets/Invincible Word Mark.png';
+import EventLogoSpanish from '../assets/Invincible_SPANISH.png';
+import EventLogoPortuguese from '../assets/Invincible_PORT.png';
 import locale from '../Data/locales.json';
 
 const Header = (props) => {
     const { language } = props;
+
+    console.log(language, 'lang');
+
     return (
         <header>
             <div className="company-name">
-                <img src={EventLogo} alt="Invincible event logo"/>
-                <h1>{locale[language]['CALA SUPERHERO NAME GENERATOR']}</h1>
+              {language === 'english' ? <img src={EventLogoEnglish} alt="Invincible event logo" /> : language === 'spanish' ? <img src={EventLogoSpanish} alt="Invincible event logo" /> : <img src={EventLogoPortuguese} alt="Invincible event logo"/>
+              }
+              <h1>{locale[language]['CALA SUPERHERO NAME GENERATOR']}</h1>
             </div>
         </header>
     );
